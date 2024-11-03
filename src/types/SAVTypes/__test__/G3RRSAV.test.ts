b3a93bd46f0c44b1a3033951e23ce0823f9a9f57
evanwporter <evanwporter@gmail.com>
2024-11-03 11:36:35 -0800

    Added tests

--- src/types/SAVTypes/__test__/G3RRSAV.test.ts
+++ src/types/SAVTypes/__test__/G3RRSAV.test.ts
@@ -2,6 +2,21 @@ import { readFileSync } from 'fs';
 import { resolve } from 'path';
 import { G3RRSAV } from '../G3RRSAV';
 import { ParsedPath } from '../path';
+import { PK3RR } from '../../../../../pokemon-files-js/src';
+import { OHPKM } from '../../pkm/OHPKM';
+
+function display_mon(mon: PK3RR | OHPKM) {
+  console.log('Boxmon:', {
+    nickname: mon.nickname,
+    // level: firstPokemon.level,
+    trainerName: mon.trainerName,
+    // trainerID: firstPokemon.trainerID,
+    heldItemIndex: mon.heldItemIndex,
+    languageIndex: mon.languageIndex,
+    dexNum: mon.dexNum,
+    moves: mon.moves,
+  });
+}
 
 describe('G3SAV - Radical Red Save Tests', () => {
   let radicalRedSave: G3RRSAV;
@@ -29,33 +44,56 @@ describe('G3SAV - Radical Red Save Tests', () => {
   });
 
   test('should print the first Pokémon in the first box', () => {
-    const firstBox = radicalRedSave.boxes[0];  // Access the first box
-    const firstPokemon = firstBox.pokemon[0];  // Access the first Pokémon in the box
+    const firstPokemon = radicalRedSave.boxes[0].pokemon[0];
 
     if (firstPokemon) {
-      console.log('First Pokémon in first box:', {
-        nickname: firstPokemon.nickname,
-        // level: firstPokemon.level,
-        trainerName: firstPokemon.trainerName,
-        // trainerID: firstPokemon.trainerID,
-        heldItemIndex: firstPokemon.heldItemIndex,
-        languageIndex: firstPokemon.languageIndex,
-        dexNum: firstPokemon.dexNum
-      });
 
-      console.log(firstBox.pokemon[1]?.nickname)
+      display_mon(firstPokemon);
       
-      // Optionally, add assertions to check properties if known
-      expect(firstPokemon).toBeDefined();
       expect(firstPokemon.nickname).toBe("Von");
       expect(firstPokemon.trainerID).toBe(10334);
       expect(firstPokemon.trainerName).toBe("Radical")
-
-      expect(firstBox.pokemon[1].nickname).toBe("Pint");
-      expect(firstBox.pokemon[1].trainerName).toBe("Radical")
-
+      expect(firstPokemon.moves[0]).toBe(33); // Tackle
+      expect(firstPokemon.moves[1]).toBe(336); // Howl
+      expect(firstPokemon.dexNum).toBe(261);
     } else {
-      console.log('No Pokémon found in the first box.');
+      console.log('No Pokémon found in the first box, first slot.');
+    }
+
+    const secondPokemon = radicalRedSave.boxes[1].pokemon[1];
+
+    if (secondPokemon) {
+      display_mon(secondPokemon)
+
+      expect(secondPokemon.nickname).toBe("Cin");
+      expect(secondPokemon.trainerID).toBe(10334);
+      expect(secondPokemon.trainerName).toBe("Radical")
+    } else {
+      console.log('No Pokémon found in the second box, second slot.');
+    }
+
+    const sevii_lokix = radicalRedSave.boxes[0].pokemon[3];
+
+    if (sevii_lokix) {
+      display_mon(sevii_lokix)
+
+      expect(sevii_lokix.nickname).toBe("Cin");
+      expect(sevii_lokix.trainerID).toBe(10334);
+      expect(sevii_lokix.trainerName).toBe("Radical")
+    } else {
+      console.log('Sevii Lokix not found.');
+    }
+
+    const lastpokemon = radicalRedSave.boxes[0].pokemon[29];
+
+    if (lastpokemon) {
+      display_mon(lastpokemon)
+
+      expect(lastpokemon.nickname).toBe("Crabrawler");
+      expect(lastpokemon.trainerID).toBe(10334);
+      expect(lastpokemon.trainerName).toBe("Radical")
+    } else {
+      console.log('No Pokémon found in the second box, second slot.');
     }
   });
 });
