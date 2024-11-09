ad824e32f33427b9fcab1e27a819f5050fcc306d
evanwporter <evanwporter@gmail.com>
2024-11-09 10:18:19 -0800

    Modified Aestherics of Save Tile Menu * Change icon to be consistent with `Open` icon on Left Hand Side * Move Remove button to right hand side, this makes more sense to me

--- src/renderer/components/Icons.tsx
+++ src/renderer/components/Icons.tsx
@@ -1,9 +1,11 @@
-import { IoGrid } from 'react-icons/io5'
-import { MdClose, MdCreateNewFolder, MdGrid4X4, MdMoreVert } from 'react-icons/md'
+import { IoGrid, IoCloseCircleOutline } from 'react-icons/io5'
+import { MdCreateNewFolder, MdGrid4X4, MdMoreVert } from 'react-icons/md'
+
+
 
 export const AddFolderIcon = MdCreateNewFolder
 export const MenuIcon = MdMoreVert
-export const RemoveIcon = MdClose
+export const RemoveIcon = IoCloseCircleOutline
 
 export const GridIcon = MdGrid4X4
 export const CardsIcon = IoGrid
