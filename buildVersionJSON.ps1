fa841ac701d82152e1f8edcee4e69a4adb9a9aa7
evanwporter <evanwporter@gmail.com>
2024-11-09 10:38:24 -0800

    Created a `npm run dev` command for windows, along with a corresponding powerscript. New commands are: * `npm run dev:unix` * `npm run dev:win`

---
+++ buildVersionJSON.ps1
@@ -0,0 +1,14 @@
+$OPENHOME_VERSION = (Get-Content -Path './package.json' | ConvertFrom-Json).version
+$GIT_COMMIT = (git rev-parse HEAD 2>$null) -replace "`n",""
+$GIT_BRANCH = (git rev-parse --abbrev-ref HEAD 2>$null) -replace "`n",""
+$GIT_TREE = if (git diff-index --quiet HEAD -- 2>$null) { "" } else { "-dirty" }
+$BUILD_DATE = (Get-Date -Format "yyyy-MM-ddTHH:mm:ssZ")
+$GIT_STATUS = "$GIT_BRANCH-$GIT_COMMIT$GIT_TREE"
+
+$versionData = @{
+    version = $OPENHOME_VERSION
+    commit = $GIT_STATUS
+    build_date = $BUILD_DATE
+}
+
+$versionData | ConvertTo-Json | Out-File -Encoding utf8 "./src/consts/JSON/version.json"
