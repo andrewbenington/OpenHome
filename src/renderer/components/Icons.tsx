a584e24f4b90c776f8122c8b5f3918a05ec76da9
Evan <115374841+evanwporter@users.noreply.github.com>
2024-11-09 12:25:37 -0800

    Undid previous changes

--- src/renderer/components/Icons.tsx
+++ src/renderer/components/Icons.tsx
@@ -1,11 +1,9 @@
-import { IoGrid, IoCloseCircleOutline } from 'react-icons/io5'
-import { MdCreateNewFolder, MdGrid4X4, MdMoreVert } from 'react-icons/md'
-
-
+import { IoGrid } from 'react-icons/io5'
+import { MdClose, MdCreateNewFolder, MdGrid4X4, MdMoreVert } from 'react-icons/md'
 
 export const AddFolderIcon = MdCreateNewFolder
 export const MenuIcon = MdMoreVert
-export const RemoveIcon = IoCloseCircleOutline
+export const RemoveIcon = MdClose
 
 export const GridIcon = MdGrid4X4
 export const CardsIcon = IoGrid
