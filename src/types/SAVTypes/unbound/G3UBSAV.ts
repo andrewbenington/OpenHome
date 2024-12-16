04c06dc1fd77bd79d072e699cf720c5a085fa9c4
evanwporter <evanwporter@gmail.com>
2024-12-16 12:47:45 -0800

    Barebones Unbound Skeleton

---
+++ src/types/SAVTypes/unbound/G3UBSAV.ts
@@ -0,0 +1,35 @@
+import { GameOfOrigin } from 'pokemon-resources'
+import { OHPKM } from '../../pkm/OHPKM'
+import { PathData } from '../path'
+import { Box, BoxCoordinates, PluginSAV } from '../SAV'
+import PK3UB from './PK3UB'
+
+export class G3UBSAV implements PluginSAV<PK3UB> {
+  origin: GameOfOrigin = 0
+  boxRows!: number
+  boxColumns!: number
+  filePath!: PathData
+  fileCreated?: Date | undefined
+  money!: number
+  name!: string
+  tid!: number
+  sid?: number | undefined
+  displayID!: string
+  currentPCBox!: number
+  boxes: Box<PK3UB>[] = []
+  bytes!: Uint8Array<ArrayBufferLike>
+  invalid: boolean = false
+  tooEarlyToOpen: boolean = false
+  pcChecksumOffset?: number | undefined
+  pcOffset?: number | undefined
+  updatedBoxSlots: BoxCoordinates[] = []
+  gameColor!: () => string
+  isPlugin: true = true
+  getPluginIdentifier!: (() => string | undefined) & (() => string)
+  getCurrentBox!: () => Box<PK3UB>
+  supportsMon!: (dexNumber: number, formeNumber: number) => boolean
+  prepareBoxesAndGetModified!: () => OHPKM[]
+  calculateChecksum?: (() => number) | undefined
+  getGameName!: () => string
+  getExtraData?: (() => object) | undefined
+}
