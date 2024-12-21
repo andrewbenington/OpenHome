5dee308df64b965d1f389d97b5307ed99414f522
Andrew Benington <andrewbenington57@gmail.com>
2024-12-21 17:21:41 -0600

    displayError hook + error popup styling

--- src/app/ErrorMessage.tsx
+++ src/app/ErrorMessage.tsx
@@ -1,12 +1,4 @@
-import {
-  Alert,
-  DialogContent,
-  DialogTitle,
-  Divider,
-  Modal,
-  ModalClose,
-  ModalDialog,
-} from '@mui/joy'
+import { Alert, DialogContent, DialogTitle, Modal, ModalClose, ModalDialog } from '@mui/joy'
 import { useContext } from 'react'
 import { ErrorIcon } from 'src/components/Icons'
 import { ErrorContext } from '../state/error'
@@ -21,14 +13,16 @@ export default function ErrorMessageModal() {
     >
       <ModalDialog style={{ padding: 8 }}>
         <ModalClose />
-        <DialogTitle>{errorState.messageData?.title}</DialogTitle>
-        <Divider />
+        <DialogTitle style={{ marginBottom: 8, fontSize: 20 }}>
+          {errorState.messageData?.title}
+        </DialogTitle>
         <DialogContent>
           {errorState.messageData?.messages?.map((msg, i) => (
             <Alert
               startDecorator={<ErrorIcon style={{ width: 20, height: 20 }} />}
               color="danger"
-              size="sm"
+              size="lg"
+              sx={{ padding: 1, fontWeight: 'bold' }}
               variant="solid"
               key={`alert_${i}`}
             >
