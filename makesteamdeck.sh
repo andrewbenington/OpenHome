c0872c4adea91944d4e60c0141dd0d2350012d85
Andrew Benington <andrewbenington57@gmail.com>
2025-06-09 16:57:10 -0500

    update script to use bash

--- makesteamdeck.sh
+++ makesteamdeck.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/sh
+#!/bin/bash
 
 docker run --rm -ti \
  --env-file <(env | grep -iE 'DEBUG|NODE_|ELECTRON_|YARN_|NPM_|CI|CIRCLE|TRAVIS_TAG|TRAVIS|TRAVIS_REPO_|TRAVIS_BUILD_|TRAVIS_BRANCH|TRAVIS_PULL_REQUEST_|APPVEYOR_|CSC_|GH_|GITHUB_|BT_|AWS_|STRIP|BUILD_') \
