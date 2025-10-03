2648c144df3bad9705812e35a259d478d20fe0b9
Evan Porter <115374841+evanwporter@users.noreply.github.com>
2025-10-03 10:32:48 -0700

    fix

--- src/state/openSaves.ts
+++ src/state/openSaves.ts
@@ -377,7 +377,7 @@ export const openSavesReducer: Reducer<OpenSavesState, OpenSavesAction> = (
       let sourceMon = sourceBox.pokemon[source.box_slot]
       let destMon = destBox.pokemon[dest.box_slot]
 
-      if (sourceMon !== source.mon) return state // necessary in strict mode, otherwise the swap will happen twice and revert
+      if (sourceMon !== source.mon) return state
       if (sourceSave !== destSave) {
         if (sourceMon) {
           if (!(sourceMon instanceof OHPKM)) {
