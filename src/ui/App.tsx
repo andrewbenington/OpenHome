131fc5a00377fd96c6bb51e9f9a09b897ca75a32
Andrew Benington <andrewbenington57@gmail.com>
2025-12-23 15:19:38 -0600

    replace reducer with simpler state

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -23,7 +23,7 @@ import { loadPlugin } from '@openhome-ui/util/plugin'
 import { Flex, Text, Theme } from '@radix-ui/themes'
 import * as E from 'fp-ts/lib/Either'
 import { useCallback, useContext, useEffect, useReducer, useState } from 'react'
-import { filterReducer, MonStateContext } from 'src/ui/state/monDisplay'
+import { initialMonDisplayState, MonDisplayState, MonStateContext } from 'src/ui/state/monDisplay'
 import './App.css'
 import AppTabs from './AppTabs'
 import useDebounce from './hooks/useDebounce'
@@ -83,7 +83,7 @@ function AppWithBackend() {
   const [mouseState, mouseDispatch] = useReducer(mouseReducer, { shift: false })
   const [dragMonState, dragMonDispatch] = useReducer(dragMonReducer, { mode: 'mon' })
   const [appInfoState, appInfoDispatch] = useReducer(appInfoReducer, appInfoInitialState)
-  const [filterState, filterDispatch] = useReducer(filterReducer, { filter: {} })
+  const [monDisplayState, setMonDisplayState] = useState<MonDisplayState>(initialMonDisplayState())
   const [pluginState, pluginDispatch] = useReducer(pluginReducer, { plugins: [], loaded: false })
   const [settingsLoading, setSettingsLoading] = useState(false)
   const [bagState, bagDispatch] = useReducer(itemBagReducer, {
@@ -183,7 +183,7 @@ function AppWithBackend() {
                 <SavesProvider>
                   <DragMonContext.Provider value={[dragMonState, dragMonDispatch]}>
                     <PokemonDragContextProvider>
-                      <MonStateContext.Provider value={[filterState, filterDispatch]}>
+                      <MonStateContext.Provider value={[monDisplayState, setMonDisplayState]}>
                         {settingsLoading ? (
                           <Flex width="100%" height="100vh" align="center" justify="center">
                             <Text size="9" weight="bold">
--- src/ui/state/monDisplay.ts
+++ src/ui/state/monDisplay.ts
@@ -1,59 +1,33 @@
 import { Filter } from '@openhome-ui/util/filter'
-import { Dispatch, Reducer, createContext, useContext } from 'react'
+import { createContext, useContext } from 'react'
 
-export type MonDisplayState = { filter: Filter }
+export type MonDisplayState = { filter: Filter; extraIndicator?: ExtraIndicatorType }
 
-export type MonDisplayAction =
-  | {
-      type: 'set_filter'
-      payload: MonDisplayState
-    }
-  | {
-      type: 'clear_filter'
-      payload?: undefined
-    }
-
-export const filterReducer: Reducer<MonDisplayState, MonDisplayAction> = (
-  state: MonDisplayState,
-  action: MonDisplayAction
-) => {
-  const { type, payload } = action
-
-  switch (type) {
-    case 'set_filter': {
-      const newState = { ...state, ...payload }
-
-      if ('dexNumber' in payload && !payload.dexNumber) {
-        newState.filter.formeNumber = undefined
-      }
-
-      return newState
-    }
-    case 'clear_filter': {
-      return initialMonDisplayState()
-    }
-  }
-}
-
-function initialMonDisplayState() {
+export function initialMonDisplayState() {
   return { filter: {} }
 }
 
-export const MonStateContext = createContext<[MonDisplayState, Dispatch<MonDisplayAction>]>([
+export const MonStateContext = createContext<[MonDisplayState, (state: MonDisplayState) => void]>([
   initialMonDisplayState(),
   () => null,
 ])
 
 export function useMonDisplay() {
-  const [state, dispatchState] = useContext(MonStateContext)
+  const [state, setState] = useContext(MonStateContext)
 
   function setFilter(newFilter: Partial<Filter>) {
-    dispatchState({ type: 'set_filter', payload: { filter: newFilter } })
+    setState({ ...state, filter: newFilter })
   }
 
   function clearFilter() {
-    dispatchState({ type: 'clear_filter' })
+    setState({ ...state, filter: {} })
   }
 
-  return { ...state, setFilter, clearFilter }
+  function setExtraIndicatorType(extraIndicator: ExtraIndicatorType | undefined) {
+    setState({ ...state, extraIndicator })
+  }
+
+  return { ...state, setFilter, clearFilter, setExtraIndicatorType }
 }
+
+export type ExtraIndicatorType = 'GameOfOrigin' | 'Gender' | 'EVs' | 'IVs'
