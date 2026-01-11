80138e5ef614f185df30b85a4742f37af0e4ab27
Andrew Benington <andrewbenington57@gmail.com>
2026-01-11 16:09:33 -0600

    polyfills for tests

--- src/test-setup.ts
+++ src/test-setup.ts
@@ -4,7 +4,10 @@ import { beforeAll } from 'vitest'
 
 import init from '../pkm_rs_resources/pkg/pkm_rs_resources.js'
 
+import { doNecessaryPolyfills } from './core/util/polyfill.js'
+
 beforeAll(async () => {
+  doNecessaryPolyfills()
   const wasmPath = path.resolve(__dirname, '../pkm_rs_resources/pkg/pkm_rs_resources_bg.wasm')
   const wasmBytes = fs.readFileSync(wasmPath)
   await init({ module_or_path: wasmBytes })
