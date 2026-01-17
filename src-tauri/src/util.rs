070a1428d8a115ddf505782bb6241d9aa05b827e
Andrew Benington <andrewbenington57@gmail.com>
2026-01-16 18:16:20 -0600

    cfg not linux

--- src-tauri/src/util.rs
+++ src-tauri/src/util.rs
@@ -7,13 +7,14 @@ use std::path::Path;
 use std::process::Command;
 use std::{collections::HashSet, path::PathBuf};
 use tauri::Manager;
-use tauri_plugin_dialog::{DialogExt, MessageDialogKind};
 use zip::ZipArchive;
 
 use crate::error::{Error, Result};
 
 #[cfg(target_os = "linux")]
 use dialog::DialogBox;
+#[cfg(not(target_os = "linux"))]
+use tauri_plugin_dialog::{DialogExt, MessageDialogKind};
 
 #[derive(serde::Serialize, serde::Deserialize, Debug)]
 pub struct ImageResponse {
@@ -306,6 +307,7 @@ pub fn open_directory(directory_path: &Path) -> Result<()> {
 }
 
 pub fn show_error_dialog(_app: &tauri::App, message: impl Into<String>, title: impl Into<String>) {
+    #[cfg(not(target_os = "linux"))]
     _app.dialog()
         .message(message)
         .title(title)
