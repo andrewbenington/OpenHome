8ecee7f7cad75a06c4a87f0675d2b5284f4a98ba
ArtemisWinter <artemiswinter.dev@protonmail.com>
2026-01-20 15:33:01 -0500

    Update filter.ts

--- src/ui/util/filter.ts
+++ src/ui/util/filter.ts
@@ -12,6 +12,7 @@ export interface Filter {
   type1?: Type
   type2?: Type
   gameOfOrigin?: OriginGame
+  nature?: NatureIndex
   ribbon?: string
   shiny?: string
   ball?: number
@@ -50,6 +51,9 @@ export function filterApplies(filter: Filter, mon: PKMInterface) {
     return false
   }
   if (filter.gameOfOrigin !== undefined && mon.gameOfOrigin !== filter.gameOfOrigin) {
+    return false
+  }
+  if (filter.nature !== undefined && mon.nature !== filter.nature) {
     return false
   }
   if (filter.ribbon !== undefined) {
