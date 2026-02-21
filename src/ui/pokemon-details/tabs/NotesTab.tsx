c9ce8f0a420a73d238f770ec4ac64993a59e9bdb
Andrew Benington <andrewbenington57@gmail.com>
2026-02-21 12:09:09 -0600

    fix launch warning bug + slow notes

--- src/ui/pokemon-details/tabs/NotesTab.tsx
+++ src/ui/pokemon-details/tabs/NotesTab.tsx
@@ -2,6 +2,7 @@ import { OHPKM } from '@openhome-core/pkm/OHPKM'
 import { useSaves } from '@openhome-ui/state/saves'
 import { TextArea } from '@radix-ui/themes'
 import { useEffect, useState } from 'react'
+import useDebounce from 'src/ui/hooks/useDebounce'
 
 export default function NotesDisplay(props: { mon: OHPKM }) {
   const { mon } = props
@@ -12,14 +13,19 @@ export default function NotesDisplay(props: { mon: OHPKM }) {
     setNotesText(mon.notes ?? '')
   }, [mon])
 
+  const debouncedNotesUpdate = useDebounce((notes: string) => {
+    updateMonNotes(mon.getHomeIdentifier(), notes)
+  }, 500)
+
   return (
     <div style={{ padding: 8, height: 'calc(100% - 16px)' }}>
       <TextArea
         value={notesText}
         onChange={(e) => {
           setNotesText(e.target.value)
-          updateMonNotes(mon.getHomeIdentifier(), e.target.value ?? '')
+          debouncedNotesUpdate(e.target.value)
         }}
+        onBlur={() => updateMonNotes(mon.getHomeIdentifier(), notesText ?? '')}
         style={{ height: '100%' }}
       />
     </div>
