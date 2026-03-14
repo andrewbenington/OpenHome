fb216b56b3ea0b2e9fd619fa09f93ec47f242b06
Andrew Benington <andrewbenington57@gmail.com>
2026-03-14 10:00:57 -0400

    location converter WIP

--- packages/pokemon-files/src/conversion/converter.ts
+++ packages/pokemon-files/src/conversion/converter.ts
@@ -1,3 +1,4 @@
+import { Generation, OriginGame, OriginGames } from '@pkm-rs/pkg'
 import { MonFormat } from '../../../../src/core/pkm/interfaces'
 import { OHPKM } from '../../../../src/core/pkm/OHPKM'
 import { ConversionStrategy, DefaultConversionStrategy, FullConversionStrategy } from './settings'
@@ -37,5 +38,51 @@ export class PkmConverter {
           return speciesName
       }
     }
+  }
+
+  metLocation(ohpkm: OHPKM): number | undefined {
+    if (OriginGames.generation(ohpkm.gameOfOrigin) === Generation.G4) {
+      if (this.format === 'PK4') {
+        return ohpkm.metLocationIndex
+      }
+    }
+  }
+}
+
+function originMatchesFormat(gameOfOrigin: OriginGame, format: MonFormat): boolean {
+  const generation = OriginGames.generation(gameOfOrigin)
+  switch (format) {
+    case 'PK1':
+      return generation === Generation.G1
+    case 'PK2':
+      return generation === Generation.G2
+    case 'PK3':
+      return generation === Generation.G3
+    case 'COLOPKM':
+      return generation === Generation.G3
+    case 'XDPKM':
+      return generation === Generation.G3
+    case 'PK4':
+      return generation === Generation.G4
+    case 'PK5':
+      return generation === Generation.G5
+    case 'PK6':
+      return generation === Generation.G6
+    case 'PK7':
+      return generation === Generation.G7
+    case 'PB7':
+      return generation === Generation.G7
+    case 'PK8':
+      return generation === Generation.G8
+    case 'PA8':
+      return generation === Generation.G8
+    case 'PB8':
+      return generation === Generation.G8
+    case 'PK9':
+      return generation === Generation.G9
+    case 'PA9':
+      return generation === Generation.G9
+    default:
+      throw new Error(`Unknown format ${format}`)
   }
 }
