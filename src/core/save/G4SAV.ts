780db69ad6a2de4660564e64f47eb2aee543e8bb
Andrew Benington <andrewbenington57@gmail.com>
2026-03-20 16:36:06 -0500

    handle gen 4 saves when the only valid block is the second one

--- src/core/save/G4SAV.ts
+++ src/core/save/G4SAV.ts
@@ -8,6 +8,7 @@ import { gen4StringToUTF } from '@openhome-core/save/util/Strings/StringConverte
 import { OriginGame } from '@pkm-rs/pkg'
 import { PK4 } from '@pokemon-files/pkm'
 import { OHPKM } from '../pkm/OHPKM'
+import { Option } from '../util/functional'
 import { Box, BoxAndSlot, OfficialSAV } from './interfaces'
 import { LookupType } from './util'
 import { PathData } from './util/path'
@@ -66,7 +67,22 @@ export abstract class G4SAV extends OfficialSAV<PK4> {
       this.origin = OriginGame.Diamond
       return
     }
-    this.origin = bytes[0x80]
+    const possibleOrigin = bytes[0x80]
+    if (G4SAV.includesOrigin(possibleOrigin)) {
+      this.origin = possibleOrigin
+    } else {
+      this.origin = 0
+    }
+  }
+
+  getCurrentSaveCount(blockOffset: number, blockSize: number): Option<number> {
+    const storedCount = bytesToUint32LittleEndian(
+      this.bytes,
+      blockOffset + blockSize - this.footerSize
+    )
+
+    // if the game has only been saved once, the second block will be full of 0xffffffff, so we return -1 to indicate it has never been saved before
+    return storedCount === 0xffffffff ? undefined : storedCount
   }
 
   buildBoxes() {
@@ -105,6 +121,12 @@ export abstract class G4SAV extends OfficialSAV<PK4> {
           console.error(`G4SAV: ${e}`)
         }
       }
+    }
+
+    if (this.origin === 0) {
+      this.origin = this.filePath.raw.toLocaleLowerCase().includes('pearl')
+        ? OriginGame.Pearl
+        : OriginGame.Diamond
     }
   }
 
