9da2c975ef97d5719faf60101199513cc5ac8e80
Andrew Benington <andrewbenington57@gmail.com>
2026-05-14 17:24:38 -0500

    missed something

--- packages/pokemon-files/src/pkm/PKM.ts
+++ packages/pokemon-files/src/pkm/PKM.ts
@@ -12,7 +12,6 @@ import PB8 from './PB8'
 import PK1 from './PK1'
 import PK2 from './PK2'
 import PK3 from './PK3'
-import PK3JavaScript from './PK3JavaScript'
 import PK4 from './PK4'
 import PK5 from './PK5'
 import PK6 from './PK6'
@@ -25,7 +24,6 @@ export type PKM =
   | PK1
   | PK2
   | PK3
-  | PK3JavaScript
   | COLOPKM
   | XDPKM
   | PK4
