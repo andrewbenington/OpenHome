2bbd09f3e32ac002f4d85ddaed76c04a2e0ce257
Andrew Benington <andrewbenington57@gmail.com>
2026-06-23 18:56:25 -0500

    fix alcremie sprites

--- src/ui/images/box-icons.test.ts
+++ src/ui/images/box-icons.test.ts
@@ -1,10 +1,13 @@
+import { SWEETS } from '@openhome-core/resources/consts/Forms'
 import { R } from '@openhome-core/util/functional'
 import { boxIconImagePath } from '@openhome-ui/pokemon-details/useBoxIconImage'
-import { all_species_data } from '@pkm-rs/pkg/pkm_rs'
+import { all_species_data, NationalDex } from '@pkm-rs/pkg/pkm_rs'
 import { existsSync } from 'fs'
+import { describe } from 'node:test'
 import { resolve } from 'path'
 import { expect, test } from 'vitest'
 import { iconType } from './images'
+import { getPokemonSpritePath } from './pokemon'
 
 test('all box icons are present', () => {
   const allFormData = all_species_data()
@@ -39,28 +42,59 @@ test('all box icons are present', () => {
   expect(missingSprites).toStrictEqual([])
 })
 
-// test('all home sprites are present', () => {
-//   const allFormData = all_species_data()
-//   const projectRoot = process.cwd()
+describe('all home sprites are present', () => {
+  test('all home sprites are present (non-Alcremie)', () => {
+    const allFormData = all_species_data()
+    const projectRoot = process.cwd()
 
-//   const missingSprites: string[] = []
+    const missingSprites: string[] = []
 
-//   for (let species of allFormData) {
-//     for (let form of species.forms.filter(
-//       (f) => iconType(species.nationalDex, f.formIndex, undefined) === 'image'
-//     )) {
-//       const spritePath = getPokemonSpritePath({
-//         dexNum: species.nationalDex,
-//         formNum: form.formIndex,
-//         format: 'OHPKM',
-//         extraFormIndex: undefined,
-//       })
-//       const absolutePath = resolve(projectRoot, 'public', spritePath)
-//       if (!existsSync(absolutePath)) {
-//         missingSprites.push(spritePath)
-//       }
-//     }
-//   }
+    for (let species of allFormData) {
+      for (let form of species.forms.filter(
+        (f) => iconType(species.nationalDex, f.formIndex, undefined) === 'image'
+      )) {
+        const spritePath = getPokemonSpritePath({
+          dexNum: species.nationalDex,
+          formNum: form.formIndex,
+          format: 'OHPKM',
+          extraFormIndex: undefined,
+        })
+        const absolutePath = resolve(projectRoot, 'public', spritePath)
+        if (!existsSync(absolutePath)) {
+          missingSprites.push(spritePath)
+        }
+      }
+    }
 
-//   expect(missingSprites).toStrictEqual([])
-// })
+    expect(missingSprites).toStrictEqual([])
+  })
+
+  test('all home sprites are present for Alcremie sweets', () => {
+    const allFormData = all_species_data()
+    const projectRoot = process.cwd()
+
+    const missingSprites: string[] = []
+
+    const species = allFormData[NationalDex.Alcremie]
+
+    for (let form of species.forms.filter(
+      (f) => iconType(species.nationalDex, f.formIndex, undefined) === 'image'
+    )) {
+      for (const sweet of Object.values(SWEETS).filter((s) => typeof s !== 'string')) {
+        const spritePath = getPokemonSpritePath({
+          dexNum: species.nationalDex,
+          formNum: form.formIndex,
+          format: 'OHPKM',
+          extraFormIndex: undefined,
+          formArgument: sweet,
+        })
+        const absolutePath = resolve(projectRoot, 'public', spritePath)
+        if (!existsSync(absolutePath)) {
+          missingSprites.push(spritePath)
+        }
+      }
+    }
+
+    expect(missingSprites).toStrictEqual([])
+  })
+})
