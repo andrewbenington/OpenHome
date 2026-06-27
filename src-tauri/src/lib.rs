c5c923b4c3f7db518eb57c08bb036d17d9903d48
Andrew Benington <andrewbenington57@gmail.com>
2026-06-27 11:20:55 -0500

    remember window state

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -23,6 +23,7 @@ use crate::state::synced_state::AllSyncedState;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_window_state::Builder::new().build())
         .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_fs::init())
         .setup(|app| {
