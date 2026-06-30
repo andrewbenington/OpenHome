3f2c33ea0483015d6821348082b3faa0d0b66184
Andrew Benington <andrewbenington57@gmail.com>
2026-06-30 15:51:33 -0500

    functional + backend updates

--- src/ui/backend/index.ts
+++ src/ui/backend/index.ts
@@ -1,7 +1,8 @@
 import { R } from '@openhome-core/util/functional'
+import BackendInterface from './backendInterface'
 import { TauriBackend } from './tauri/backend'
 
-export const AppBackend = R.expect(
+export const AppBackend: BackendInterface = R.expect(
   await TauriBackend.start(),
   (error) => `error starting Tauri backend: ${error}`
 )
