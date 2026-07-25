1c0735567754a0710cce48a4a3f64c1fe54edc20
Andrew Benington <andrewbenington57@gmail.com>
2026-07-25 17:10:27 -0500

    remove old code generation

--- src/core/save/radicalred/G3RRSAV.test.ts
+++ src/core/save/radicalred/G3RRSAV.test.ts
@@ -1,16 +1,23 @@
 import { NationalDexMax } from '@openhome-core/resources/consts/NationalDex'
 import { supportsMon } from '@openhome-core/save/util'
 import { range } from '@openhome-core/util/functional'
-import { Language, Lookup } from '@pkm-rs/pkg'
+import { Language, Lookup, NationalDex } from '@pkm-rs/pkg'
 import { expect, test } from 'vitest'
 import { G3RRSAV } from './G3RRSAV'
 
-test(`Pokémon Radical Red supports all mon base forms`, () => {
+test(`Pokémon Radical Red supports all mon base forms aside from Maushold`, () => {
   for (const i of range(NationalDexMax)) {
     const nationalDex = i + 1
-    expect(
-      supportsMon(G3RRSAV, nationalDex, 0),
-      `Pokémon ${Lookup.speciesName(nationalDex, Language.English)} is transferrable to Radical Red`
-    ).toBe(true)
+    if (nationalDex !== NationalDex.Maushold) {
+      expect(
+        supportsMon(G3RRSAV, nationalDex, 0),
+        `Pokémon ${Lookup.speciesName(nationalDex, Language.English)} (${nationalDex}) is transferrable to Radical Red`
+      ).toBe(true)
+    } else {
+      expect(
+        supportsMon(G3RRSAV, nationalDex, 1),
+        `Pokémon ${Lookup.speciesName(nationalDex, Language.English)} (${nationalDex}) is transferrable to Radical Red`
+      ).toBe(true)
+    }
   }
 })
