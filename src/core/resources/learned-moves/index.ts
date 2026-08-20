fe949dfb344600255ed4585fe00af5ac8a601428
Andrew Benington <andrewbenington57@gmail.com>
2026-08-20 17:43:04 -0500

    LZA move flags updated on game sync

--- src/core/resources/learned-moves/index.ts
+++ src/core/resources/learned-moves/index.ts
@@ -1,4 +1,9 @@
-export * from './swsh-trs'
 export * from './bdsp-tms'
 export * from './la-tutors'
 export * from './sv-tms'
+export * from './swsh-trs'
+
+export const LZA_BASE_TM_BYTES = 25
+export const LZA_DLC_TM_BYTES = 13
+export const LZA_PLUS_MOVES_BLOCK_C_BYTES = 33
+export const LZA_PLUS_MOVES_BLOCK_D_BYTES = 12
