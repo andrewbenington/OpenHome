a32a0a7cce1c7ff40794c7b1e9a2b89dac80f907
Andrew Benington <andrewbenington57@gmail.com>
2026-08-25 21:16:58 -0500

    fix grid column widths + hover column transparency

--- src/ui/columns/ohpkm.tsx
+++ src/ui/columns/ohpkm.tsx
@@ -65,13 +65,25 @@ export default function useOhpkmColumns(
     {
       key: 'type1',
       name: 'Type 1',
-      width: '3.5rem',
+      width: '4rem',
       sortFunction: stringSorter((mon) => mon.metadata?.type1),
       renderValue: (mon) =>
         mon.metadata?.type1Index !== undefined ? (
           <TypeIcon size="1.5rem" typeIndex={mon.metadata?.type1Index} />
         ) : null,
       getFilterValue: (mon) => mon.metadata?.type1 || 'Unknown',
+      cellClass: 'centered-cell',
+    },
+    {
+      key: 'type2',
+      name: 'Type 2',
+      width: '4rem',
+      sortFunction: stringSorter((mon) => mon.metadata?.type2),
+      renderValue: (mon) =>
+        mon.metadata?.type2Index !== undefined ? (
+          <TypeIcon size="1.5rem" typeIndex={mon.metadata?.type2Index} />
+        ) : null,
+      getFilterValue: (mon) => mon.metadata?.type2 || 'Unknown',
       cellClass: 'centered-cell',
     },
     {
@@ -209,7 +221,7 @@ export default function useOhpkmColumns(
     {
       key: 'level',
       name: 'Level',
-      width: '3rem',
+      width: '5rem',
       renderValue: (value) => value.getLevel(),
       getFilterValue: (mon) => getLevelRange(mon.getLevel()),
       getFilterValueDropdownPos: (filterValue) =>
@@ -220,7 +232,7 @@ export default function useOhpkmColumns(
     {
       key: 'hp',
       name: 'HP',
-      width: '2.5rem',
+      width: '3.5rem',
       renderValue: (mon) => mon.stats.hp.toString(),
       getFilterValue: (mon) => mon.stats.hp.toString(),
       sortFunction: numericSorter((mon) => mon.stats.hp),
@@ -230,7 +242,7 @@ export default function useOhpkmColumns(
     {
       key: 'atk',
       name: 'ATK',
-      width: '2.5rem',
+      width: '3.5rem',
       renderValue: (mon) => mon.stats.atk.toString(),
       getFilterValue: (mon) => mon.stats.atk.toString(),
       sortFunction: numericSorter((mon) => mon.stats.atk),
@@ -240,7 +252,7 @@ export default function useOhpkmColumns(
     {
       key: 'def',
       name: 'DEF',
-      width: '2.5rem',
+      width: '3.5rem',
       renderValue: (mon) => mon.stats.def.toString(),
       getFilterValue: (mon) => mon.stats.def.toString(),
       sortFunction: numericSorter((mon) => mon.stats.def),
@@ -250,7 +262,7 @@ export default function useOhpkmColumns(
     {
       key: 'spa',
       name: 'SPA',
-      width: '2.5rem',
+      width: '3.5rem',
       renderValue: (mon) => mon.stats.spa.toString(),
       getFilterValue: (mon) => mon.stats.spa.toString(),
       sortFunction: numericSorter((mon) => mon.stats.spa),
@@ -260,7 +272,7 @@ export default function useOhpkmColumns(
     {
       key: 'spd',
       name: 'SPD',
-      width: '2.5rem',
+      width: '3.5rem',
       renderValue: (mon) => mon.stats.spd.toString(),
       getFilterValue: (mon) => mon.stats.spd.toString(),
       sortFunction: numericSorter((mon) => mon.stats.spd),
@@ -270,7 +282,7 @@ export default function useOhpkmColumns(
     {
       key: 'spe',
       name: 'SPE',
-      width: '2.5rem',
+      width: '3.5rem',
       renderValue: (mon) => mon.stats.spe.toString(),
       getFilterValue: (mon) => mon.stats.spe.toString(),
       sortFunction: numericSorter((mon) => mon.stats.spe),
