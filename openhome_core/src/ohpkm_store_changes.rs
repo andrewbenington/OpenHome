4976a558327e31fd80e662adcf344e5f27d53df6
merlinnerd922 <richardchen922@gmail.com>
2026-09-02 09:26:49 -0400

    IN-PROGRESS, NON-COMPILING: starting to create the idea of a state change

--- openhome_core/src/ohpkm_store_changes.rs
+++
@@ -1,7 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-#[derive(Default, Serialize, Deserialize, Clone)]
-pub struct OhpkmStoreChanges {
-
-
-}
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -10,7 +10,8 @@ mod state;
 mod synced_state;
 mod util;
 mod version;
-mod box_pointer;
+
+
 
 use crate::data_controller::ToDataController;
 use crate::synced_state::LazyState;
@@ -20,6 +21,8 @@ use openhome_core::{Error, Result};
 use std::env;
 use tauri::Manager;
 use openhome_core::pkm_storage::{load_banks, StoredBankData};
+use openhome_core::box_pointer::BoxPointer;
+use openhome_core::ohpkm_store_partial::OhpkmStorePartial;
 
 const RAW_HANDLER: fn(tauri::ipc::Invoke<tauri::Wry>) -> bool = tauri::generate_handler![
     commands::get_file_bytes,
@@ -140,7 +143,8 @@ pub fn run() {
             };
 
             let synced_state =
-                LazyState::from_partial_states(lookup_state, get_bank_pointer(bank_info), conversion_settings);
+                LazyState::from_partial_states(lookup_state, get_bank_pointer(bank_info),
+                                               conversion_settings);
             app.manage(synced_state);
 
             let pokedex_state = match state::PokedexState::load_from_storage(&controller) {
