923ddfaaef699358093be4bfecbb601ecc8f12c4
Andrew Benington <andrewbenington57@gmail.com>
2026-09-07 14:16:57 -0500

    fix for gen 2

--- src/core/pkm/PK1.ts
+++ src/core/pkm/PK1.ts
@@ -150,12 +150,14 @@ export default class PK1 {
         spe: 0,
         spc: 0,
       }
+
       this.dvs = other.dvs
       this.dvs.hp =
         ((this.dvs.atk & 1) << 3) |
         ((this.dvs.def & 1) << 2) |
         ((this.dvs.spe & 1) << 1) |
         (this.dvs.spc & 1) // old OHPKMs calculated this incorrectly
+
       this.trainerName = other.trainerName
       this.nickname = converter.nickname(other)
     }
--- src/core/pkm/PK2.ts
+++ src/core/pkm/PK2.ts
@@ -154,6 +154,7 @@ export default class PK2 {
         spe: 0,
         spc: 0,
       }
+
       this.dvs = other.dvs ?? {
         hp: 0,
         atk: 0,
@@ -161,6 +162,12 @@ export default class PK2 {
         spe: 0,
         spc: 0,
       }
+      this.dvs.hp =
+        ((this.dvs.atk & 1) << 3) |
+        ((this.dvs.def & 1) << 2) |
+        ((this.dvs.spe & 1) << 1) |
+        (this.dvs.spc & 1) // old OHPKMs calculated this incorrectly
+
       this.trainerFriendship = other.trainerFriendship ?? 0
       this.pokerusByte = other.pokerusByte ?? 0
       this.metTimeOfDay = other.metTimeOfDay ?? 0
