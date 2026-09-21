261ec5075cc08d71e932c3f420f864cc569c2c6a
merlinnerd922 <richardchen922@gmail.com>
2026-09-21 16:48:18 -0400

    Adding EmEx compatibility files by basing them off the compatibility files from other ROM hacks

--- pkm_rs/src/rom_hacks/emerald_expansion/mod.rs
+++ pkm_rs/src/rom_hacks/emerald_expansion/mod.rs
@@ -1,5 +1,6 @@
 mod species_convert;
 
 pub mod emerald_expansion;
+mod pk3emex;
 
 use species_convert::{NATIONAL_DEX_TO_EMERALD_EX_MAP, EMERALD_EX_TO_NATIONAL_DEX_MAP};
---
+++ pkm_rs/src/rom_hacks/emerald_expansion/pk3emex.rs
@@ -0,0 +1,9 @@
+use pkm_rs_resources::species::SpeciesForm;
+use serde::Serialize;
+
+
+use crate::result::{Error, NdexConvertSource, Result};
+
+#[cfg(feature = "randomize")]
+use pkm_rs_types::randomize::Randomize;
+
