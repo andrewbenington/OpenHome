57e6c732a0bc58c8fb8e4cb18971359951341c19
Andrew Benington <andrewbenington57@gmail.com>
2026-09-21 13:18:44 -0400

    add missing await

--- src/ui/state/drag-and-drop/PokemonDndContext.tsx
+++ src/ui/state/drag-and-drop/PokemonDndContext.tsx
@@ -9,6 +9,7 @@ import {
 } from '@dnd-kit/core'
 import { displayIndexAdder, isBattleFormeItem, isMegaStone } from '@openhome-core/pkm/util'
 import { monSupportedBySave } from '@openhome-core/save/util'
+import { R } from '@openhome-core/util/functional'
 import PokemonIcon from '@openhome-ui/components/PokemonIcon'
 import useDisplayError from '@openhome-ui/hooks/displayError'
 import { getPublicImageURL } from '@openhome-ui/images/images'
@@ -214,7 +215,7 @@ export default function PokemonDndContext(props: { children?: ReactNode }) {
 
               await savesAndBanks
                 .moveMon({ ...sourceLoc, mon: currMon }, nextDestination)
-                .catch((error) => displayError('Error moving Pokémon', error))
+                .then(R.mapErr((error) => displayError('Could not move Pokémon', error)))
 
               nextDestination = nextDestination.isHome
                 ? nextHomeDestination(nextDestination.box, nextDestination.boxSlot + 1)
@@ -232,9 +233,9 @@ export default function PokemonDndContext(props: { children?: ReactNode }) {
               savesAndBanks.moveMonItemToBag(source)
             }
 
-            savesAndBanks
+            await savesAndBanks
               .moveMon(source, dest)
-              .catch((error) => displayError('Could not move Pokémon', error))
+              .then(R.mapErr((error) => displayError('Could not move Pokémon', error)))
           }
         }
 
@@ -243,7 +244,6 @@ export default function PokemonDndContext(props: { children?: ReactNode }) {
       onDragStart={(e) => {
         const payload = e.active.data?.current
         if (!isDragPayload(payload)) return
-        console.log('starting to drag', payload)
         startDragging(payload)
       }}
       onDragOver={onDragOver}
