22b467c9129171f5468965a8b90e560c00fc1168
merlinnerd922 <richardchen922@gmail.com>
2026-09-23 13:49:10 -0400

    Fixing last of the compilation issues

--- pkm_rs/src/rom_hacks/mod.rs
+++ pkm_rs/src/rom_hacks/mod.rs
@@ -1,10 +1,3 @@
-// mod cfru;
-// pub mod rr;
-// pub mod ub;
-
-mod emerald_expn;
-mod rr;
-
 #[derive(Debug, Clone, Copy, serde::Serialize, serde::Deserialize, specta::Type)]
 #[serde(rename_all = "snake_case")]
 pub enum PluginIdentifier {
